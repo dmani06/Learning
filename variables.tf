730da8d82fb00f0836fb09f7d7ce399b83fed62d
dmani06 <34037698+dmani06@users.noreply.github.com>
2017-12-06 12:51:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,12 @@
 # VPC variables
-variable "AWS_REGION" {
+variable "access_key" {
+  default = ""
+}
+
+variable "secret_key" {
+  default = ""
+}
+variable "aws_region" {
   default = "us-east-2"
 }
 
